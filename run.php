706692614b2e64dd0df4e4493fb3f52c3889ae4c
Gustavo Mena <gustavomenacba@gmail.com>
2022-04-09 00:31:46 -0300

    Remove files

--- run.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-// $command = escapeshellcmd('python /domains/fenixrepuestos.com/public_html/test/command.py');
-// $output = shell_exec($command);
-
-$salida = shell_exec('ls');
-echo $salida;
